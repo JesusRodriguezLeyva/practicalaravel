491b2ec0c705a5528a4824f3aa7e3819aec24f61
Jesus Rodriguez <rodriguezleyva012@gmail.com>
2025-02-16 17:24:42 -0600

    main

---
+++ server.php
@@ -0,0 +1,13 @@
+<?php
+
+$port = getenv('PORT') ?: 8000;
+
+if (php_sapi_name() == 'cli-server') {
+    $url = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url["path"];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
+require_once __DIR__ . '/public/index.php';
